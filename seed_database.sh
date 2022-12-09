572da213a7180e1bff561d337725acfcaa821d9a
Scott Parks <swparks040@gmail.com>
2022-12-09 13:59:12 -0600

    migrated db.sqlite3, created seed databse.sh file, added cmoapi fixtures (7), commit BEFORE loaddata

---
+++ seed_database.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm db.sqlite3
+rm -rf ./cmoapi/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations cmoapi
+python3 manage.py migrate cmoapi
+python3 manage.py loaddata categories
+python3 manage.py loaddata cmousers
+python3 manage.py loaddata family_member_relationships
+python3 manage.py loaddata family_members
+python3 manage.py loaddata messages
+python3 manage.py loaddata pto
+python3 manage.py loaddata responses
