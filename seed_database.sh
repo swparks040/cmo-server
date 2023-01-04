fca1a263f561bb5a72671813caee4d1dd8a44d1c
Scott Parks <swparks040@gmail.com>
2023-01-04 14:30:25 -0600

    successfully seeded database

--- seed_database.sh
+++ seed_database.sh
@@ -1,12 +1,14 @@
-!/bin/bash
 rm db.sqlite3
 rm -rf ./cmoapi/migrations
-python3 manage.py migrate
 python3 manage.py makemigrations cmoapi
-python3 manage.py migrate cmoapi
-python3 manage.py loaddata categories
-python3 manage.py loaddata pto
+python3 manage.py migrate
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+python3 manage.py loaddata cmousers
 python3 manage.py loaddata family_member_relationships
 python3 manage.py loaddata family_members
+python3 manage.py loaddata categories
+python3 manage.py loaddata pto
+python3 manage.py loaddata pto_requests
 python3 manage.py loaddata messages
-# python3 manage.py loaddata responses
+python3 manage.py loaddata comments
